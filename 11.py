a73a66a5919d6102e3355c903fe0f9eb6ba90506
pkr2805 <137922268+pkr2805@users.noreply.github.com>
2024-12-13 20:31:42 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 
+current_year = datetime.now().year
+df_patient['birthDate'] = pd.to_datetime(df_patient['birthDate'], errors='coerce')
+df_patient['age'] = current_year - df_patient['birthDate'].dt.year
+
+
+df_consultation['medicine_count'] = df_consultation['medicines'].apply(len)
+
+df_combined = df_patient.merge(df_consultation, left_on='_id', right_on='_id', how='inner')
+
+correlation = df_combined['medicine_count'].corr(df_combined['age'])
+print(round(correlation, 2)) 
