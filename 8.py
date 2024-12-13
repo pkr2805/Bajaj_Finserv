43791d8f99bb8cf8112979a1aff082bd932298bd
pkr2805 <137922268+pkr2805@users.noreply.github.com>
2024-12-13 20:28:10 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,1 +1,6 @@
+all_medicines = [medicine['medicineName'] for entry in data for medicine in entry['consultationData']['medicines']]
 
+# Count frequency of each medicineName
+medicine_frequency = pd.Series(all_medicines).value_counts()
+third_most_frequent = medicine_frequency.index[2]
+print(third_most_frequent)
